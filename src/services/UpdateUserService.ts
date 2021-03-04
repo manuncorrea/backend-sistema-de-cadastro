add58e4edad1b9c4ef8664986a2c86ce269c603e
manuncorrea <manuncorrea@hotmail.com>
2021-03-04 16:55:11 -0300

    Update UpdateUserService.ts

--- src/services/UpdateUserService.ts
+++ src/services/UpdateUserService.ts
@@ -40,30 +40,3 @@ export default UpdateUserService
 
 
 
-// import User from '../models/User';
-
-// interface AdressProps{
-//   state: string;
-//   city: string;
-//   neighborhood: string;
-//   street: string;
-//   number: string;
-// }
-
-// interface UserProps {
-//   firstName: string;
-//   lastName: string;
-//   address: AdressProps;
-//   phone: string;
-// }
-
-// class UpdateUserServices {
-//   public async execute({ firstName, lastName, address, phone }: UserProps) {
-//     const user = await User.update({ firstName, lastName, address, phone})
-
-//     return user;
-//   }
-   
-// }
-
-// export default UpdateUserServices;
