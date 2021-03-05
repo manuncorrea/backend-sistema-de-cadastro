b08676a1dc0b4bd0326ec526ec80232599176516
manuncorrea <manuncorrea@hotmail.com>
2021-03-05 08:33:28 -0300

    Update server.ts
    up

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,6 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Industria Freios Supremos server started');
+  console.log('Sistema de cadastro de clientes server started');
 });
 
